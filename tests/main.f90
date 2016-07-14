54e13e81732f92bafd31f70d938b951eefd1d6a2
AbelHeinsbroek <mail@abelheinsbroek.nl>
2016-07-14 12:44:17 +0200

    Initial checking IPHREEQC 3.3.7

---
+++ tests/main.f90
@@ -0,0 +1,24 @@
+!  main.f90 
+!
+!  FUNCTIONS:
+!  main - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: test_f90
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program test_f90
+
+    implicit none
+    
+    integer(KIND=4) F_MAIN
+    integer(KIND=4) I
+    
+    I = F_MAIN()
+
+    end program test_f90
